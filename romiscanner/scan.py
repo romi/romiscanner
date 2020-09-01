158d1c1f98006fffb84bc503e1dda3349541c9dd
jcharlaix <ju.charlaix@gmail.com>
2020-09-01 15:37:03 +0200

    update calibration

--- romiscanner/scan.py
+++ romiscanner/scan.py
@@ -168,7 +168,7 @@ class VirtualScan(Scan):
         return vscan
 
 
-class CalibrationScan(RomiTask):
+class CalibrationScan(Scan):
     """ A task for running a scan, real or virtual, with a calibration path.
 
     Module: romiscan.tasks.scan
@@ -191,5 +191,6 @@ class CalibrationScan(RomiTask):
 
     def run(self):
         path = Scan().get_path()
-        calibration_path = path(CalibrationScan, self.n_points_line)
+        path_module = importlib.import_module(ScanPath().module)
+        calibration_path = getattr(path_module, "CalibrationScan")(path, self.n_points_line)
         Scan().run(path=calibration_path)
